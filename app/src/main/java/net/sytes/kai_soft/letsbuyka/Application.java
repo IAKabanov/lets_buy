299efea73f4673792836e46a73d4edaad346f432
LKogtie <ivankab1996@gmail.com>
2018-05-13 09:29:28 +0300

    Fragments work!

---
+++ app/src/main/java/net/sytes/kai_soft/letsbuyka/Application.java
@@ -0,0 +1,12 @@
+package net.sytes.kai_soft.letsbuyka;
+
+/**
+ * Created by Лунтя on 08.05.2018.
+ */
+
+public class Application extends android.app.Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
--- app/src/main/java/net/sytes/kai_soft/letsbuyka/ProductModel/ProductsActivity.java
+++ app/src/main/java/net/sytes/kai_soft/letsbuyka/ProductModel/ProductsActivity.java
@@ -1,6 +1,5 @@
 package net.sytes.kai_soft.letsbuyka.ProductModel;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -14,26 +13,26 @@ import net.sytes.kai_soft.letsbuyka.R;
 public class ProductsActivity extends AppCompatActivity implements
         IProductListActivityContract {
 
-    DetailFragment detailFragment;
-    ListFragment listFragment;
-    FragmentManager fragmentManager;
-
+    DetailFragment detailFragment;      //Фрагмент детализации
+    ListFragment listFragment;          //Фрагмент списка
+    FragmentManager fragmentManager;    //Фрагмент менеджер
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_products_list);
+
+        //Создали фрагменты
         fragmentManager = getSupportFragmentManager();
         detailFragment = new DetailFragment();
         listFragment = new ListFragment();
 
-        // начинаем транзакцию
+        //Начинаем транзакцию
         FragmentTransaction ft = fragmentManager.beginTransaction();
-        // Создаем и добавляем первый фрагмент
-        ListFragment listFragment = new ListFragment();
+        //Создаем и добавляем первый фрагмент
         ft.add(R.id.activityProductsList, listFragment, "listFragment");
-        // Подтверждаем операцию
+        //Подтверждаем операцию
         ft.commit();
     }
 
