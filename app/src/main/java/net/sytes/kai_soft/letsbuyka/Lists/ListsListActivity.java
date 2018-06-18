bdd1801e15f38a695114f379e79c5cdbd1f85706
LKogtie <ivankab1996@gmail.com>
2018-06-18 13:40:03 +0300

    Pre-alpha 0.7.1
    -fixed: quick fixes

--- app/src/main/java/net/sytes/kai_soft/letsbuyka/Lists/ListsListActivity.java
+++ app/src/main/java/net/sytes/kai_soft/letsbuyka/Lists/ListsListActivity.java
@@ -1,5 +1,6 @@
 package net.sytes.kai_soft.letsbuyka.Lists;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -11,6 +12,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 
 import net.sytes.kai_soft.letsbuyka.CustomList.CustomActivity;
@@ -78,6 +80,7 @@ public class ListsListActivity extends AppCompatActivity implements IListsListAc
     public void onBackPressed() {
         //refreshToolbar(nameFragment.peek());
         onListDetailFragmentButtonClick();
+        hideKeyboard();
         super.onBackPressed();
     }
 
@@ -147,6 +150,9 @@ public class ListsListActivity extends AppCompatActivity implements IListsListAc
     @Override
     public void onListListItemLongClick(List list) {
 
+        this.list = list;
+
+
 
         // начинаем транзакцию
         FragmentTransaction ft = fragmentManager.beginTransaction();
@@ -159,10 +165,10 @@ public class ListsListActivity extends AppCompatActivity implements IListsListAc
         detailFragment.setArguments(bundle);
 
         ft.replace(R.id.activityListsList, detailFragment, "detailFragment");
+        ft.addToBackStack("detailFragment");
         // Подтверждаем операцию
         ft.commit();
         nameFragment.push("detailFragment");
-        this.list = list;
         refreshToolbar();
 
         /*actionSave.setVisible(true);
@@ -274,5 +280,10 @@ public class ListsListActivity extends AppCompatActivity implements IListsListAc
                 break;
 
         }
+    }
+
+    private void hideKeyboard(){
+        InputMethodManager imm = (InputMethodManager) getApplicationContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(toolBar.getWindowToken(), 0);
     }
 }
