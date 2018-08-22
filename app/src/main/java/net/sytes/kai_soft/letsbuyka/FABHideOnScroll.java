e23ea0d54d207c398cb85ce150d764843d52707d
IKabanov2 <ivankab1996@gmail.com>
2018-08-22 16:26:36 +0300

    - FAB is broken

--- app/src/main/java/net/sytes/kai_soft/letsbuyka/FABHideOnScroll.java
+++ app/src/main/java/net/sytes/kai_soft/letsbuyka/FABHideOnScroll.java
@@ -2,6 +2,7 @@ package net.sytes.kai_soft.letsbuyka;
 
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.ViewCompat;
@@ -17,20 +18,18 @@ public class FABHideOnScroll extends FloatingActionButton.Behavior {
     public FABHideOnScroll(Context context, AttributeSet attrs) {
         super();
     }
+
+
+
     @Override
     public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child,
                                View target, int dxConsumed, int dyConsumed,
-                               int dxUnconsumed, int dyUnconsumed) {
+                               int dxUnconsumed, int dyUnconsumed, int type) {
         super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed,
-                dxUnconsumed, dyUnconsumed);
+                dxUnconsumed, dyUnconsumed, type);
         //child -> Floating Action Button
-/*
-        if (child.getVisibility() == View.VISIBLE && dyConsumed > 0) {
-            child.hide();
-        } else if (child.getVisibility() == View.GONE && dyConsumed < 0) {
-            child.show();
-        }
-*/
+
+
 
         if (dyConsumed > 0) {
             CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams)
@@ -44,10 +43,11 @@ public class FABHideOnScroll extends FloatingActionButton.Behavior {
 
 
     }
+
     @Override
     public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout,
                                        FloatingActionButton child, View directTargetChild,
-                                       View target, int nestedScrollAxes) {
+                                       View target, int nestedScrollAxes, int type) {
         return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL;
     }
 }
