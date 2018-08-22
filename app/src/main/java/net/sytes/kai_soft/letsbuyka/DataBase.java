6d57e100ba2a34999465915e45ad968a132226ae
IKabanov2 <ivankab1996@gmail.com>
2018-08-22 09:44:22 +0300

    Added a few comments in DataBase class

--- app/src/main/java/net/sytes/kai_soft/letsbuyka/DataBase.java
+++ app/src/main/java/net/sytes/kai_soft/letsbuyka/DataBase.java
@@ -11,13 +11,16 @@ import android.util.Log;
  * Created by Лунтя on 14.04.2018.
  */
 
+
+/*  Синглтоновый класс. Здесь происходит создание, получение и апгрейд бд   */
 public class DataBase extends SQLiteOpenHelper {
-    public static final String DATABASE_NAME = "LetsBuy.db";
+    private static final String DATABASE_NAME = "LetsBuy.db";
 
     public static final String TABLE_NAME_PRODUCTS_LIST = "products";
     public static final String TABLE_NAME_LISTS_LIST = "lists";
     public static final String TABLE_NAME_CUSTOM_LIST = "customList";
 
+    private static final String TAG = "DataBase";
 
     private static DataBase instance;
     private static Context mContext;
@@ -26,45 +29,37 @@ public class DataBase extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, 1);
     }
 
-
-/*    public boolean insert(String itemName, String description, String photo){
-        ContentValues cv = new ContentValues();
-
-        cv.put(TABLE_ITEM_NAME, itemName);
-        cv.put(TABLE_DESCRIPTION, description);
-        cv.put(TABLE_PHOTO, photo);
-
-
-        return true;
-    } */
-
     public static DataBase getInstance(Context context){
-        Log.e("DB", "got");
+
+        Log.i(TAG, "got instance");
         mContext = context;
         if (instance == null){
             instance = new DataBase(context);
         }
         return instance;
+
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
 
         DataBase.tableProducts.createTableProducts(db);
-        Log.e("DB", "products created");
+        Log.i(TAG, "products created");
         DataBase.tableLists.createTableList(db);
-        Log.e("DB", "lists created");
+        Log.i(TAG, "lists created");
         DataBase.tableCustomList.createTableCustomList(db);
-        Log.e("DB", "cust list created");
-
+        Log.i(TAG, "cust list created");
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        Log.e("DB", "upgraded");
+        Log.i("DB", "upgraded");
     }
 
+
+    /*  Таблица продуктов. При создании добавляется стандартный набор продуктов.
+    *   Стандартный набор лежит в res\values\strings\productsList   */
     public static class tableProducts{
         public static final String TABLE_ID = "_ID";
         public static final String TABLE_ITEM_NAME = "name";
@@ -72,31 +67,24 @@ public class DataBase extends SQLiteOpenHelper {
         public static final String TABLE_PHOTO = "photos";
 
         private static void createTableProducts(SQLiteDatabase db){
+            //  Создание таблицы
             db.execSQL("create table " + TABLE_NAME_PRODUCTS_LIST + " ( "
                     + DataBase.tableProducts.TABLE_ID + " integer primary key autoincrement, "
                     + DataBase.tableProducts.TABLE_ITEM_NAME + " text, "
                     + DataBase.tableProducts.TABLE_DESCRIPTION + " text, "
                     + DataBase.tableProducts.TABLE_PHOTO + " text " + " ); ");
-            String[] productsArr = mContext.getResources().getStringArray(R.array.productsList);
 
+            //  Заполнение стандартным набором
+            String[] productsArr = mContext.getResources().getStringArray(R.array.productsList);
             for (String aProductsArr : productsArr) {
                 ContentValues cv = new ContentValues();
                 cv.put(DataBase.tableProducts.TABLE_ITEM_NAME, aProductsArr);
                 db.insert(TABLE_NAME_PRODUCTS_LIST, null, cv);
             }
         }
-        public static void a1(DataBase db){
-            SQLiteDatabase bd = db.getWritableDatabase();
-            for (int i = 0; i < 5; i++) {
-                ContentValues cv = new ContentValues();
-                cv.put(tableCustomList.TABLE_ID_LIST, 1);
-                cv.put(tableCustomList.TABLE_ID_PRODUCT, i+1);
-                bd.insert(TABLE_NAME_CUSTOM_LIST, null, cv);
-            }
-
-        }
     }
 
+    /*  Таблица со списками. Создание   */
     public static class tableLists{
         public static final String TABLE_ID = "_ID";
         public static final String TABLE_ITEM_NAME = "name";
@@ -109,11 +97,24 @@ public class DataBase extends SQLiteOpenHelper {
 
     }
 
+    /*  Таблица с созданным пользователем списком   */
     public static class tableCustomList{
         public static final String TABLE_ID = "_id";
         public static final String TABLE_ID_PRODUCT = "id_product";
         public static final String TABLE_ID_LIST = "id_list";
         public static final String TABLE_DEPRECATED = "deprecated";
+
+        /*  Синтетическое заполнение для дебага. Принимает id списка и id продукта, начиная с
+            которого добавится count последующих продуктов   */
+        public static void a1(DataBase db, int idList, int idProduct, int count){
+            SQLiteDatabase bd = db.getWritableDatabase();
+            for (int i = 0; i < count; i++) {
+                ContentValues cv = new ContentValues();
+                cv.put(tableCustomList.TABLE_ID_LIST, idList);
+                cv.put(tableCustomList.TABLE_ID_PRODUCT, idProduct + i);
+                bd.insert(TABLE_NAME_CUSTOM_LIST, null, cv);
+            }
+        }
 
         private static void createTableCustomList(SQLiteDatabase db){
             db.execSQL("create table " + TABLE_NAME_CUSTOM_LIST + " ( "
